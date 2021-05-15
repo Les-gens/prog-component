c1efdb39169875105fb2060d0b45b3c235b139d6
Francois Lannoy <flannoy154@gmail.com>
2021-05-15 22:03:53 +0200

    :poop: WIP profile page

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,7 +1,25 @@
-import react from "react"
+import { User } from "models/User"
+import React, { useEffect, useState } from "react"
 
-const Profile = () => {
-  return <div>tototoot</div>
+interface Props {
+  user: User
+}
+const Profile = ({ user }: Props) => {
+  const [friend_list, setFriendList] = useState<User[]>([])
+  useEffect(() => {
+    setFriendList((prevFriends) => [...prevFriends, new User(1, "toto")])
+    setFriendList((prevFriends) => [...prevFriends, new User(2, "titi")])
+  }, [friend_list])
+  return (
+    <div className="w-full h-full">
+      <h1></h1>
+      {friend_list !== [] && friend_list[0] !== undefined ? (
+        <div id="friends_list">{friend_list[0].username}</div>
+      ) : (
+        ""
+      )}
+    </div>
+  )
 }
 
 export default Profile
