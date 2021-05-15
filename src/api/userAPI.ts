3fe6cc8711748508999831b6b9105a46960a0f03
Galimede <mat.degand@outlook.fr>
2021-05-15 21:56:43 +0200

    :construction: Add basic API function utils

---
+++ src/api/userAPI.ts
@@ -0,0 +1,29 @@
+async function getAllUsers () {
+
+}
+
+async function getUser (userID: Number) {
+
+}
+
+async function updateUser (userID: Number, userInformation: Object) {
+
+}
+
+async function deleteUser (userID: Number) {
+
+}
+
+async function authUser (username: String, password: String) {
+
+}
+
+async function addFriend ( userID: Number, friendID : Number) {
+
+}
+
+async function acceptFriendRequest ( userID: Number, friendID: Number) {
+
+}
+
+export { getAllUsers, getUser, updateUser, deleteUser, authUser, addFriend, acceptFriendRequest }
