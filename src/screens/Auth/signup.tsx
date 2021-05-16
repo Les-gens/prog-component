ea024413409c99bb2e82ca46402206d4c7b924e1
Galimede <mat.degand@outlook.fr>
2021-05-16 16:26:51 +0200

    :art: Fromat login and signup form

--- src/screens/Auth/signup.tsx
+++ src/screens/Auth/signup.tsx
@@ -2,30 +2,30 @@ import React from 'react'
 
 export default function SignUp () {
   return (
-        <div className="w-full max-w-xs">
+    <div className="w-full max-w-xs">
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
             <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
                     Username
-  </label>
+                </label>
                 <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"></input>
-</div>
-                <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                        Password
-  </label>
-                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"></input>
-                        <p className="text-red-500 text-xs italic">Please choose a password.</p>
-</div>
-                    <div className="flex items-center justify-between">
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                            Sign Up
-  </button>
-  <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
-        ALready have an acoount ? Sign In
-      </a>
-                    </div>
-</form>
             </div>
+            <div className="mb-6">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                    Password
+                </label>
+                <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"></input>
+                 <p className="text-red-500 text-xs italic">Please choose a password.</p>
+            </div>
+            <div className="flex items-center justify-between">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                    Sign Up
+                </button>
+                <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
+                    ALready have an acoount ? Sign In
+                </a>
+            </div>
+        </form>
+    </div>
   )
 }
