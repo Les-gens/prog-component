ac88273b61458bdcebab4f3965758273de550731
Francois Lannoy <flannoy154@gmail.com>
2021-05-17 10:42:47 +0200

    :sparkles: added call to user/:id post

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,24 +1,30 @@
 import { getUser, getUserFriends, getUserPosts } from 'api/userAPI'
-import { Post as PostModel} from 'models/Post.js'
 import { User } from 'models/User'
 import React, { useEffect, useState } from 'react'
 import {FriendListItem} from './components/index.js'
 import NavBar from 'screens/Home/navbar'
+import { getAllPosts } from 'api/postAPI'
+import {Post} from 'screens/index'
 
 const Profile = () => {
   const [friend_list, setFriendList] = useState<User[]>([])
-  const [post_list, setPostList] = useState<PostModel[]>([])
+  const [post_list, setPostList] = useState<any[]>([])
   const [user, setUser] = useState<User>()
 
   useEffect(() => {
     async function fetchData(){
       const user = await getUser(1)
+      const posts = await getAllPosts()
+      if (posts){
+        const postFormatted = posts.filter((post: { user_id: number }) => post.user_id === 1)
+        setPostList(postFormatted)
+      }
       setUser(user)
     }
     fetchData()
 
     setFriendList(getUserFriends(1))
-    setPostList(getUserPosts(1))
+    // setPostList(getUserPosts(1))
   }, [])
   return (
     <div className="w-full h-full">
@@ -39,7 +45,7 @@ const Profile = () => {
         <div className='flex flex-col w-8/12 items-center mt-6'>
           <h1 className='text-3xl'>Posts</h1>
           {post_list !== [] ?
-            post_list.map( (post, idx) => <div className='mt-6 border-2 border-gray-200 w-30 h-28' key={'post_'+idx}><div>{post.title}</div><div>{post.content}</div></div>)
+            post_list.map( (post, idx) => <Post key={'post_'+idx} title={post.title} content={post.text} isLoading={false}></Post>)
           : ''}
         </div>
         <div id='friend_list' className='w-2/12 flex flex-col self-end'>
