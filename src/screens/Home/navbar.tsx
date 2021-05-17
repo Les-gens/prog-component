2dc8383e6cfd46a56566f06c069c61bfe89d06e0
Francois Lannoy <flannoy154@gmail.com>
2021-05-17 10:45:00 +0200

    :children_crossing: added button to home

--- src/screens/Home/navbar.tsx
+++ src/screens/Home/navbar.tsx
@@ -47,9 +47,7 @@ export default function NavBar (props: navBarProps) {
           <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="flex items-center px-2 lg:px-0">
-                <div className="flex-shrink-0 font-bold text-white">
-                 Social Media
-                </div>
+                <Link to='/home' className="flex-shrink-0 font-bold text-white">Social Media</Link>
             </div>
               <div className="flex-1 flex justify-center px-2 lg:ml-6 lg:justify-end">
                 <div className="max-w-lg w-full lg:max-w-xs">
